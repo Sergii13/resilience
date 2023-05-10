e30e2015fd07d7b1524df35b553783c2c4f6126f
sergii13 <sergii.khilchuk13@gmail.com>
2023-05-10 13:28:34 +0300

    add new page

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,21 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+const allClickedElem = document.querySelectorAll(".elem-item");
+if (allClickedElem.length > 0) {
+  let indexActive = 0;
+  let timer = null;
+  allClickedElem.forEach((item, index) => {
+    item.addEventListener("click", (e) => {
+      indexActive = index;
+    });
+  });
+  timer = setInterval(() => {
+    indexActive++;
+    if (indexActive > allClickedElem.length - 1) {
+      indexActive = 0;
+    }
+    allClickedElem[indexActive].click();
+  }, 4000);
+}
